210469c1ff46f51dcf0c8f907efb0ebdce149228
Dayuan Tang <jamestang@berkeley.edu>
2019-04-22 07:25:54 -0700

    data pull from firebase in progress

---
+++ Final project/Model/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  Final project
+//
+//  Created by Jimmy Tang on 4/22/19.
+//  Copyright © 2019 徐乾智. All rights reserved.
+//
+
+import Foundation
+import FirebaseFirestore
+
+/* Structure to store user info and associated event info.*/
+var User: [String: Dictionary<String, Any>] = [:]
